0b9207c5a75c97cb605328e6d74171d54ee38408
Yonara Brito <70780854+ybrrio@users.noreply.github.com>
2020-11-30 22:57:14 -0300

    Adicionando melhorias no layout

--- Form-projeto-estacio/alteracao.php
+++ Form-projeto-estacio/alteracao.php
@@ -1,4 +1,5 @@
 
+  
 <?php
     include_once("conexao.php");
 
@@ -20,16 +21,32 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="estilos/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilos/style.css">
+    <link rel="stylesheet" href="estilos/cadastro.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>$("input").prop('required',true);</script>
     <title>tabela de users</title>
 </head>
 <body>
-    <form action="update.php" method="POST" class="alteracao">
-        <input type="hidden" name="cod" value="<?php echo $cod;?>">
-        <input type="text" name="nome" value="<?php echo $nome;?>">
-        <input type="email" name="email" value="<?php echo $email;?>">
-        <input type="tel" name="tel" value="<?php echo $tel;?>">
-        <input type="submit" value="salvar">
-    </form>
+    <div class="alinhar">
+        <form action="update.php" method="POST" class="alteracao">
+            <h3>Editar:</h3>
+            <div class="ajustes">
+                <input type="hidden" name="cod" value="<?php echo $cod;?>">
+                <p>Nome <span style="color:red;">*</span><input type="text" class="inputs" name="nome" value="<?php echo $nome;?>" required></p>
+            </div>
+            <div class="ajustes">
+                <p>Email <span style="color:red;">*</span><input type="email" class="inputs" name="email" value="<?php echo $email;?>" required></p>
+            </div>
+            <div class="ajustes">
+                <p>Telefone <span style="color:red;">*</span><input type="tel" class="inputs" name="tel" value="<?php echo $tel;?>" required></p>
+            </div>
+            <div style="padding-left: 26%;">
+                <input type="submit" class="btn btn-primary" value="salvar">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- Form-projeto-estacio/tabela.php
+++ Form-projeto-estacio/tabela.php
@@ -1,3 +1,4 @@
+
   
 <?php
 
@@ -8,91 +9,56 @@ $select = "SELECT * FROM users";
 
 <!DOCTYPE html>
 <head>
-    <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   <!-- Bootstrap CSS -->
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-   <title>Tabela de Users</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="estilos/style.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+<title>Tabela de Users</title>
+
 </head>
-<body>
-
 <style>
-    button{
-        border: 0px;
-       
-    }
-    a{
-        text-decoration: none;
-    
-
-    }
-    h4{
-        padding-left: 30px;
-       padding-top: 50px;
-       font-family: arial; 
-       font-weight: bolder;
-    }
-    .cad-usuario{
-    padding-left: 20px;
-     padding-bottom: 6px;
-     padding-right: 20px;
-     padding-top: 6px;
-     margin-top: 6px;
-    }
-    .cad-link{
-        font-weight: bolder;
-        color: white;
-        border: none;
-    }
-
     .div-cad{
         padding-left: 80%;
     }
+
 </style>
-            <h4>
-              Usuários Cadastrados
-            </h4>
-          
-<table class="table" style=" position: absolute;
-    top: 20%;
-    font-size: 20px;
-    color: rgb(49, 48, 48);
-    border-radius: 5px;
-    text-align: center;" >
+<body>
+<h4 style="padding-top: 2%">Usuários Cadastrados</h4>
+      
+<table class="table" style=" position: absolute; top: 20%; font-size: 20px; color: rgb(49, 48, 48); border-radius: 5px; text-align: center;" >
     
-   <tr class="titulos alert alert-secondary">
-      <th scope="col">Código</th>
-      <th scope="col">Nome do Usuário</th>
-      <th scope="col">Email</th>
-      <th scope="col">Telefone</th>
-      <th scope="col" colspan="2">Ações</th>
+    <tr class="titulos alert alert-secondary">
+        <th scope="col">Nome do Usuário</th>
+        <th scope="col">Email</th>
+        <th scope="col">Telefone</th>
+        <th scope="col" colspan="2">Ações</th>
     </tr>
 <?php
 $result = $conexao->query($select);
-while($tbl = mysqli_fetch_array($result)){
+while($tbl = mysqli_fetch_array($result)):
     $cod = $tbl['codigo'];
     $nome = $tbl['nome'];
     $email = $tbl['email'];
     $tel = $tbl ['tel'];
 
     echo "<tr>";
-    echo "<td>$cod</td>";
     echo "<td>$nome</td>";
     echo "<td>$email</td>";
     echo "<td>$tel</td>";
-    echo "<td> <icon><a href='alteracao.php?id=$cod' class='alt btn btn-primary'>editar</a></td></icon>";
-    echo "<td> <icon><a href='exclusao.php?id=$cod' class='alt btn btn-danger'>remover</a></td></button>";
-    echo"</tr>";
-}
-
 ?>
-    <div class="div-cad">
- <button type="button" class="btn btn-primary cad-usuario">
-         <a class="cad-link" href="cadastro.html" >Cadastrar Novo Usuário</a>
- </button>
-      </div>
-   
-      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384
+<td><a href='alteracao.php?id=<?php echo $cod; ?>' class='alt btn btn-primary'><icon class="fas fa-edit"></icon></a></td>
+<td><a href='exclusao.php?id=<?php echo $cod; ?>' class='alt btn btn-danger'><icon class="fas fa-trash-alt"></icon></a></td></button>
+</tr>
+<?php endwhile; ?>
+<div class="div-cad" style="padding-left: 85%">
+    <button type="button" class="btn btn-primary cad-usuario"><a style="color: white; text-decoration: none;" href="cadastro.html" >Incluir Usuário</a></button>
+</div>
+
 </body>
 </html>
